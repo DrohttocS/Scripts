774c8d782d38fbda2e40862dc9465d456fb133ba
Scott Hord <shc@luvbugz.net>
2023-05-16 17:48:23 -0600

    Updates

--- AD/AD Sync -/AD Sync.ps1
+++ AD/AD Sync -/AD Sync.ps1
@@ -1,5 +1,5 @@
 ﻿
 $AdminCred = Get-StoredCredential -Target "$env:USERNAME-Admin"
-$syncSVR =  Get-ADUser -LDAPFilter "(description=*configured to synchronize to tenant*)"     -Properties description | % { $_.description.SubString(142, $_.description.IndexOf(" ", 142) -142)}
+$syncSVR =  Get-ADUser -LDAPFilter "(description=*configured to synchronize to tenant*)" -Properties description | % { $_.description.SubString(142, $_.description.IndexOf(" ", 142) -142)}
 Invoke-Command -ComputerName $syncSVR  -Credential $AdminCred -ScriptBlock{
  Start-ADSyncSyncCycle -PolicyType Delta}
--- Audit/Priv groups 4 Dawnps1.ps1
+++ Audit/Priv groups 4 Dawnps1.ps1
@@ -7,9 +7,9 @@ $groups = "Enterprise Admins", "Domain Admins", "Schema Admins","DNSAdmins"
                 $file =$dtpath + '\' + $date +'-' + $group + '.txt'
                 $y = "$group is empty"
                 $x = $null           
-                $x = Get-ADGroupMember -Identity $group  -Recursive | foreach{ get-aduser $_} | select SamAccountName,objectclass,name | sort SamAccountName
+                $x = Get-ADGroupMember -Identity $group  -Recursive | foreach{ get-aduser $_ -Properties Description } | select SamAccountName,objectclass,name,Description | sort SamAccountName
                      if($X -eq $null){write-host "$group is empty"; $y| Out-File $file
-                     }else{$x; $x | Out-File $file }
+                     }else{write-host "$group"; $x; $x | Out-File $file }
 
             }
 
